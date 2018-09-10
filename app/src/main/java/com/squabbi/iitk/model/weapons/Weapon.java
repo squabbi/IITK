e40a46b50cfd806737b25db8ee8a46b67f3186a2
Squabbi <squabbi@protonmail.com>
2018-09-10 17:35:31 +1000

    Added weapon item enumeration
    Enum for the default weapon type, FlipCard extends this and adds CardType enum to it

--- app/src/main/java/com/squabbi/iitk/model/weapons/Weapon.java
+++ app/src/main/java/com/squabbi/iitk/model/weapons/Weapon.java
@@ -4,8 +4,26 @@ import com.squabbi.iitk.model.Item;
 
 public class Weapon extends Item {
     private Integer mLevel;
+    private Type mType;
+
+    public enum Type {
+        BURSTER, ULTRA_STRIKE, FLIP_CARD
+    }
+
+    Weapon (Type type, Integer level) {
+        this.mType = type;
+        this.mLevel = level;
+    }
+
+    Weapon () {
+
+    }
 
     public Integer getLevel() {
         return mLevel;
+    }
+
+    public Type getType() {
+        return mType;
     }
 }
