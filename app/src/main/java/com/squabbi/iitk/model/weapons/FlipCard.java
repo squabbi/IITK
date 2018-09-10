824f6d10999a56aa2ef3eb1a8851c3a5371a133c
Squabbi <squabbi@protonmail.com>
2018-09-10 17:39:42 +1000

    Added super's constructor

--- app/src/main/java/com/squabbi/iitk/model/weapons/FlipCard.java
+++ app/src/main/java/com/squabbi/iitk/model/weapons/FlipCard.java
@@ -3,6 +3,10 @@ package com.squabbi.iitk.model.weapons;
 public class FlipCard extends Weapon {
     private FlipType mFlipType;
 
+    FlipCard(Type type, Integer level) {
+        super(type, level);
+    }
+
     public enum FlipType {
         ADA_REFACTOR, JARVIS_VIRUS
     }
