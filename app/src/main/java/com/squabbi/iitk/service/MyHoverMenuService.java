e40f66e69a416b103d38957d8a6c74152269ddf6
Jayden <12905509@studnet.uts.edu.au>
2018-09-10 14:59:37 +1000

    Initial Android Studio commit
    Base Empty activity with google/Hover added as an implementation.

---
+++ app/src/main/java/com/squabbi/iitk/service/MyHoverMenuService.java
@@ -0,0 +1,15 @@
+package com.squabbi.iitk.service;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+import io.mattcarroll.hover.HoverView;
+import io.mattcarroll.hover.window.HoverMenuService;
+
+public class MyHoverMenuService extends HoverMenuService {
+    @Override
+    protected void onHoverMenuLaunched(@NonNull Intent intent, @NonNull HoverView hoverView) {
+        // Replace by how you want to start and configure your HoverView
+        //super.onHoverMenuLaunched(intent, hoverView);
+    }
+}
