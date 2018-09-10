53441d50ccd2202cf1af65528a5ad4fef57f6a37
Squabbi <squabbi@protonmail.com>
2018-09-10 19:42:12 +1000

    Added constructor for Item

--- app/src/main/java/com/squabbi/iitk/model/Item.java
+++ app/src/main/java/com/squabbi/iitk/model/Item.java
@@ -3,6 +3,10 @@ package com.squabbi.iitk.model;
 public class Item {
     private Rarity mRarity;
 
+    Item (Rarity rarity) {
+        this.mRarity = rarity;
+    }
+
     public enum Rarity {
         VERY_COMMON, COMMON, RARE, VERY_RARE
     }
