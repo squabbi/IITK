6d95bbd068ed57f34c6ba65104d3479f285c0244
Squabbi <squabbi@protonmail.com>
2018-09-10 16:22:03 +1000

    Added intent to start activity

--- app/src/main/java/com/squabbi/iitk/activity/MainActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.squabbi.iitk.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.squabbi.iitk.R;
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newPortalBtn = findViewById(R.id.new_portal_btn);
+        newPortalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewPortalActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
