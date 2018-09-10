27b7bbc65c9789574897f2c360fd7cef8085978f
Squabbi <squabbi@protonmail.com>
2018-09-10 15:20:13 +1000

    Added NewPortalActivity
    Base NewPortalActivity w/ FAB

---
+++ app/src/main/java/com/squabbi/iitk/activity/NewPortalActivity.java
@@ -0,0 +1,31 @@
+package com.squabbi.iitk.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.squabbi.iitk.R;
+
+public class NewPortalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_portal);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
