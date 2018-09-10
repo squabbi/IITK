1d3cffdcfcd4682d73eb9b21e10719ad28a8d2e7
Squabbi <squabbi@protonmail.com>
2018-09-10 19:32:23 +1000

    Added list of items, getter and setter for CapsuleType and numberOfItems

--- app/src/main/java/com/squabbi/iitk/model/Capsule.java
+++ app/src/main/java/com/squabbi/iitk/model/Capsule.java
@@ -1,7 +1,11 @@
 package com.squabbi.iitk.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Capsule {
     private CapsuleType mCapsuleType;
+    private List<Item> mItemLinkedList = new LinkedList<>();
 
     Capsule(CapsuleType capsuleType) {
         this.mCapsuleType = capsuleType;
@@ -13,5 +17,13 @@ public class Capsule {
 
     public CapsuleType getCapsuleType() {
         return mCapsuleType;
+    }
+
+    public void setCapsuleType(CapsuleType capsuleType) {
+        mCapsuleType = capsuleType;
+    }
+
+    public Integer numberOfItems() {
+        return mItemLinkedList.size();
     }
 }
