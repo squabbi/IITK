ad1c1db4c56928cef63a11f7118d50604d7fdf3d
Squabbi <squabbi@protonmail.com>
2018-09-10 21:15:13 +1000

    Added Place field for Portals

--- app/src/main/java/com/squabbi/iitk/model/Portal.java
+++ app/src/main/java/com/squabbi/iitk/model/Portal.java
@@ -2,13 +2,17 @@ package com.squabbi.iitk.model;
 
 import android.media.Image;
 
+import com.google.android.gms.location.places.Place;
+
 public class Portal {
     private String mName;
     private Image mImage;
+    private Place mPlace;
     // Store location here
 
-    Portal(String name, Image image) {
+    Portal(String name, Image image, Place place) {
         this.mName = name;
         this.mImage = image;
+        this.mPlace = place;
     }
 }
