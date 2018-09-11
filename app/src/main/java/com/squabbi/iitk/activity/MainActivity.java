6f4c99a476a30d47132321942a7edb242ec62640
Squabbi <squabbi@protonmail.com>
2018-09-11 20:06:42 +1000

    Removed onClick related code

--- app/src/main/java/com/squabbi/iitk/activity/MainActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/MainActivity.java
@@ -15,18 +15,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button newPortalBtn = findViewById(R.id.new_portal_btn);
-        newPortalBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, NewPortalActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    public void launchHoverService(View view) {
-        MyHoverMenuService.showFloatingMenu(this);
     }
 }
