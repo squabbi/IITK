305932a763d7151d785738fbab52cb023a04e96b
Squabbi <squabbi@protonmail.com>
2018-09-11 11:39:45 +1000

    Added MenuInflator

--- app/src/main/java/com/squabbi/iitk/activity/NewPortalActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/NewPortalActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 
@@ -46,6 +47,12 @@ public class NewPortalActivity extends AppCompatActivity {
         } catch (GooglePlayServicesNotAvailableException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_portal, menu);
+        return true;
     }
 
     @Override
