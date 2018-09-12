c4f398d6d820fd938793338a64e691fba9a59e7a
Squabbi <squabbi@protonmail.com>
2018-09-12 20:10:30 +1000

    Fixed onClickListener example

--- app/src/main/java/com/squabbi/iitk/fragment/PortalListFragment.java
+++ app/src/main/java/com/squabbi/iitk/fragment/PortalListFragment.java
@@ -7,22 +7,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.squabbi.iitk.R;
+import com.squabbi.iitk.activity.LoginActivity;
 import com.squabbi.iitk.activity.NewPortalActivity;
 
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PortalListFragment extends Fragment {
-    @OnClick(R.id.openPortal_btn)
-    public void openPortal(View view) {
-        Intent intent = new Intent(getContext().getApplicationContext(), NewPortalActivity.class);
-        startActivity(intent);
-    }
+public class PortalListFragment extends Fragment implements View.OnClickListener {
+
+    private Button mOpenPortal;
+    private Button mOpenLogin;
 
     public PortalListFragment() {
         // Required empty public constructor
@@ -32,7 +33,24 @@ public class PortalListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_portal_list, container, false);
+        final View view = inflater.inflate(R.layout.fragment_portal_list, container, false);
+        mOpenPortal = view.findViewById(R.id.openPortal_btn);
+        mOpenLogin = view.findViewById(R.id.openLogin_btn);
+
+        mOpenPortal.setOnClickListener(this);
+        mOpenLogin.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.openPortal_btn:
+                startActivity(new Intent(getContext(), NewPortalActivity.class));
+                break;
+            case R.id.openLogin_btn:
+                startActivity(new Intent(getContext(), LoginActivity.class));
+        }
+    }
 }
