056628b625a18044be8449eab2340c1537e11640
Squabbi <squabbi@protonmail.com>
2018-09-12 11:10:46 +1000

    SettingsActivityRefactor

--- app/src/main/java/com/squabbi/iitk/activity/SettingsActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/SettingsActivity.java
@@ -1,7 +1,9 @@
-package com.squabbi.iitk;
+package com.squabbi.iitk.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.squabbi.iitk.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
