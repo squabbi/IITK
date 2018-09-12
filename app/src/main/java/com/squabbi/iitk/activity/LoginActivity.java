6aac05ae485639cd000bf716b0cc998b634bf96d
Squabbi <squabbi@protonmail.com>
2018-09-12 20:15:48 +1000

    Fix Toolbar not showing name

--- app/src/main/java/com/squabbi/iitk/activity/LoginActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/LoginActivity.java
@@ -7,12 +7,17 @@ import android.support.v7.widget.Toolbar;
 import com.squabbi.iitk.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.login_activity_toolbar) Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
     }
 }
