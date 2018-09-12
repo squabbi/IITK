858bf8b5f1cb45ccd96266acea6b093e1187c30f
Squabbi <squabbi@protonmail.com>
2018-09-12 15:47:29 +1000

    Sample portal list fragment

--- app/src/main/java/com/squabbi/iitk/fragment/PortalListFragment.java
+++ app/src/main/java/com/squabbi/iitk/fragment/PortalListFragment.java
@@ -1,6 +1,7 @@
 package com.squabbi.iitk.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,12 +9,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.squabbi.iitk.R;
+import com.squabbi.iitk.activity.NewPortalActivity;
+
+import butterknife.OnClick;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PortalListFragment extends Fragment {
+    @OnClick(R.id.openPortal_btn)
+    public void openPortal(View view) {
+        Intent intent = new Intent(getContext().getApplicationContext(), NewPortalActivity.class);
+        startActivity(intent);
+    }
 
     public PortalListFragment() {
         // Required empty public constructor
