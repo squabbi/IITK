1ae8140b723807bb8bef8ee6b50ddb26762855f5
Squabbi <squabbi@protonmail.com>
2018-09-12 15:56:45 +1000

    Created Application class
    For FastSave library:
    https://github.com/yehiahd/FastSave-Android

---
+++ app/src/main/java/com/squabbi/iitk/IITKApplication.java
@@ -0,0 +1,11 @@
+package com.squabbi.iitk;
+
+import android.app.Application;
+
+public class IITKApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        
+    }
+}
