757310f9936c40c09bde758e0a3ecee7bbea6438
Squabbi <squabbi@protonmail.com>
2018-09-12 16:06:26 +1000

    Implemented FastSave
    Managing UserPreferences

--- app/src/main/java/com/squabbi/iitk/IITKApplication.java
+++ app/src/main/java/com/squabbi/iitk/IITKApplication.java
@@ -2,10 +2,12 @@ package com.squabbi.iitk;
 
 import android.app.Application;
 
+import com.appizona.yehiahd.fastsave.FastSave;
+
 public class IITKApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        
+        FastSave.init(getApplicationContext());
     }
 }
