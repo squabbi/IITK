b7c05bbbd17d45bcd7aaafec9ce1c16bf1375a3d
Squabbi <squabbi@protonmail.com>
2018-09-12 21:39:24 +1000

    Added Sign up functions
    For login, just fix up the onClickListener for the respective button
    
    #11 maybe show an alert dialog when logging in and out.

--- app/src/main/java/com/squabbi/iitk/activity/LoginActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/LoginActivity.java
@@ -1,18 +1,34 @@
 package com.squabbi.iitk.activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.squabbi.iitk.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.login_activity_toolbar) Toolbar mToolbar;
+    @BindView(R.id.email_et) EditText mEmailEt;
+    @BindView(R.id.password_et) EditText mPasswordEt;
+
+    private final String TAG = getClass().getSimpleName();
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +37,60 @@ public class LoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(mToolbar);
+
+        // Initalise FirebaseAuth service
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void createAccount(String email, String password) {
+        // Validate email and password
+        if (isValidEmail(email)) {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // Launch the main activity, it will deal with the signed in user.
+                        Toast.makeText(LoginActivity.this, "Signup success." + mAuth.getCurrentUser().getUid(),
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(LoginActivity.this, "Authentication success. " + mAuth.getCurrentUser().getUid(),
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private static boolean isValidEmail(CharSequence email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    @OnClick(R.id.login_sign_up_btn)
+    public void signUpUser() {
+        createAccount(mEmailEt.getText().toString(), mPasswordEt.getText().toString());
     }
 
     @Override
