b433d5ca3e2a3600203bd0310c51238251acc88f
Squabbi <squabbi@protonmail.com>
2018-09-12 15:52:57 +1000

    LoginActivity shell

--- app/src/main/java/com/squabbi/iitk/activity/LoginActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/LoginActivity.java
@@ -2,8 +2,11 @@ package com.squabbi.iitk.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.squabbi.iitk.R;
+
+import butterknife.BindView;
 
 public class LoginActivity extends AppCompatActivity {
 
