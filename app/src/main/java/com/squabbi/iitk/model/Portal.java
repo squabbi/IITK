bcb747a76c0be92b9a025583e635a0d574be9f5b
Squabbi <squabbi@protonmail.com>
2018-09-13 19:32:06 +1000

    Updated Portal POJO

--- app/src/main/java/com/squabbi/iitk/model/Portal.java
+++ app/src/main/java/com/squabbi/iitk/model/Portal.java
@@ -4,20 +4,14 @@ import android.media.Image;
 
 import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.IgnoreExtraProperties;
-import com.google.type.LatLng;
 
 @IgnoreExtraProperties
 public class Portal {
-
-//    public static final String FIELD_NAME = "mName";
-//    public static final String FIELD_LOCATION = "mLongLat";
-//    public static final String FIELD_NOTES = "mNotes";
 
     private String mName;
     private String mNotes;
     private GeoPoint mGeoPoint;
     //private Image mImage;
-
 
     Portal() {}
 
