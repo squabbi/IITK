26e77c417ccc56341f54f118cdb0fb0c7b40b54f
Squabbi <squabbi@protonmail.com>
2018-09-13 18:56:15 +1000

    Fixed Portal POJO with Firestore

--- app/src/main/java/com/squabbi/iitk/model/Portal.java
+++ app/src/main/java/com/squabbi/iitk/model/Portal.java
@@ -2,17 +2,61 @@ package com.squabbi.iitk.model;
 
 import android.media.Image;
 
-import com.google.android.gms.location.places.Place;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.IgnoreExtraProperties;
+import com.google.type.LatLng;
 
+@IgnoreExtraProperties
 public class Portal {
-    private String mName;
-    private Image mImage;
-    private Place mPlace;
-    // Store location here
 
-    Portal(String name, Image image, Place place) {
+//    public static final String FIELD_NAME = "mName";
+//    public static final String FIELD_LOCATION = "mLongLat";
+//    public static final String FIELD_NOTES = "mNotes";
+
+    private String mName;
+    private String mNotes;
+    private GeoPoint mGeoPoint;
+    //private Image mImage;
+
+
+    Portal() {}
+
+    public Portal(String name, com.google.android.gms.maps.model.LatLng latLng, String notes) {
         this.mName = name;
-        this.mImage = image;
-        this.mPlace = place;
+        //this.mImage = image;
+        this.mGeoPoint = new GeoPoint(latLng.latitude, latLng.longitude);
+        this.mNotes = notes;
+    }
+
+//    public Image getImage() {
+//        return mImage;
+//    }
+//
+//    public void setImage(Image image) {
+//        mImage = image;
+//    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getNotes() {
+        return mNotes;
+    }
+
+    public void setNotes(String notes) {
+        mNotes = notes;
+    }
+
+    public GeoPoint getGeoPoint() {
+        return mGeoPoint;
+    }
+
+    public void setGeoPoint(GeoPoint geoPoint) {
+        mGeoPoint = geoPoint;
     }
 }
