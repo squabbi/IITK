ed8e5d544ab62cc9d3b10c2d2865ac5654da8b66
Squabbi <squabbi@protonmail.com>
2018-09-13 15:14:27 +1000

    Cleaned up import statements

--- app/src/main/java/com/squabbi/iitk/activity/MainActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/MainActivity.java
@@ -1,14 +1,11 @@
 package com.squabbi.iitk.activity;
 
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.VisibleForTesting;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -19,17 +16,12 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.squabbi.iitk.R;
 import com.squabbi.iitk.fragment.InventoryFragment;
 import com.squabbi.iitk.fragment.PortalListFragment;
-import com.squabbi.iitk.model.Inventory;
-import com.squabbi.iitk.service.MyHoverMenuService;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -68,6 +60,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     Snackbar.LENGTH_SHORT).show();
         }
 
+//        TextView textView = mNavigationView.getHeaderView(0).findViewById(R.id.nav_header_current_user_tv);
+//        textView.setText("hello");
+
         //updateUI(currentUser);
     }
 
@@ -100,14 +95,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mPortalListFragment = new PortalListFragment();
 
         mNavigationView.setNavigationItemSelectedListener(this);
-
-        // Set the current fragment as the Portal list view
-//        updateFragment(mPortalListFragment);
-//        mNavigationView.setCheckedItem(R.id.nav_portals);
-//        setTitle(R.string.fragment_portals);
-
-        TextView textView = mNavigationView.getHeaderView(0).findViewById(R.id.nav_header_current_user_tv);
-        textView.setText("hello");
 
         if (savedInstanceState == null) {
             MenuItem item = mNavigationView.getMenu().getItem(0);
