c272f81cc284a9925266561ff11d3cc704ee34ce
Squabbi <squabbi@protonmail.com>
2018-09-18 11:10:16 +1000

    Added New Inventory activity

---
+++ app/src/main/java/com/squabbi/iitk/activity/NewInventoryActivity.java
@@ -0,0 +1,16 @@
+package com.squabbi.iitk.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.squabbi.iitk.R;
+
+public class NewInventoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_inventory);
+    }
+}
