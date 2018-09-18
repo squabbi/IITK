387607f63e80a30924613fea8708000e9c93a036
squabbi <squabbi@users.noreply.github.com>
2018-09-18 22:16:03 +1000

    Fixed ButterKnife binding

--- app/src/main/java/com/squabbi/iitk/activity/NewInventoryActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/NewInventoryActivity.java
@@ -1,16 +1,25 @@
 package com.squabbi.iitk.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 import com.squabbi.iitk.R;
+import com.squabbi.iitk.adapter.KeyLockerAdapter;
 
 public class NewInventoryActivity extends AppCompatActivity {
+
+    @BindView(R.id.lockers_gridview) GridView mLockerGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_inventory);
+        ButterKnife.bind(this);
+
+        mLockerGridView.setAdapter(new KeyLockerAdapter(this));
     }
 }
