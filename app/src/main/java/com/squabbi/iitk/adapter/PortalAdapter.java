269b6998e61c9495468953332553d9c8f19d381f
squabbi <squabbi@users.noreply.github.com>
2018-09-24 16:07:33 +1000

    Converted to use ViewModel and FirestoreQueryLiveData

--- app/src/main/java/com/squabbi/iitk/adapter/PortalAdapter.java
+++ app/src/main/java/com/squabbi/iitk/adapter/PortalAdapter.java
@@ -5,17 +5,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.squabbi.iitk.R;
 import com.squabbi.iitk.model.Portal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class PortalAdapter extends PortalFirestoreAdapter<PortalAdapter.ViewHolder> {
+public class PortalAdapter extends RecyclerView.Adapter<PortalAdapter.ViewHolder> {
+
+    private ArrayList<DocumentSnapshot> mSnapshots = new ArrayList<>();
 
     public interface OnPortalSelectedListener {
 
@@ -25,9 +35,51 @@ public class PortalAdapter extends PortalFirestoreAdapter<PortalAdapter.ViewHold
 
     private OnPortalSelectedListener mListener;
 
-    public PortalAdapter(Query query, OnPortalSelectedListener listener) {
-        super(query);
+    public PortalAdapter(OnPortalSelectedListener listener) {
         mListener = listener;
+    }
+
+    public void setData(QuerySnapshot querySnapshot) {
+        // Populate list of Documents
+        for (DocumentChange documentChange : querySnapshot.getDocumentChanges()) {
+
+            switch (documentChange.getType()) {
+                case ADDED:
+                    onDocumentAdded(documentChange);
+                    break;
+                case MODIFIED:
+                    onDocumentModified(documentChange);
+                    break;
+                case REMOVED:
+                    onDocumentRemoved(documentChange);
+                    break;
+            }
+        }
+
+        notifyDataSetChanged();
+    }
+
+    protected void onDocumentAdded(DocumentChange change) {
+        mSnapshots.add(change.getNewIndex(), change.getDocument());
+        notifyItemInserted(change.getNewIndex());
+    }
+
+    protected void onDocumentModified(DocumentChange change) {
+        if (change.getOldIndex() == change.getNewIndex()) {
+            // Item changed but remained in the same position
+            mSnapshots.set(change.getOldIndex(), change.getDocument());
+            notifyItemChanged(change.getOldIndex());
+        } else {
+            // Item changed and changed position
+            mSnapshots.remove(change.getOldIndex());
+            mSnapshots.add(change.getNewIndex(), change.getDocument());
+            notifyItemMoved(change.getOldIndex(), change.getNewIndex());
+        }
+    }
+
+    protected void onDocumentRemoved(DocumentChange change) {
+        mSnapshots.remove(change.getOldIndex());
+        notifyItemRemoved(change.getOldIndex());
     }
 
     @NonNull
@@ -39,7 +91,12 @@ public class PortalAdapter extends PortalFirestoreAdapter<PortalAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(@NonNull PortalAdapter.ViewHolder holder, int position) {
-        holder.bind(getSnapshot(position), mListener);
+        holder.bind(mSnapshots.get(position), mListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSnapshots.size();
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
