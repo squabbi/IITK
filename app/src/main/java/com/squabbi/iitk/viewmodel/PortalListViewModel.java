04529a68f8ddc572ef9654ec41f64b7f350c1873
squabbi <squabbi@users.noreply.github.com>
2018-09-24 16:06:52 +1000

    Implemented LiveData for queries

--- app/src/main/java/com/squabbi/iitk/viewmodel/PortalListViewModel.java
+++ app/src/main/java/com/squabbi/iitk/viewmodel/PortalListViewModel.java
@@ -1,10 +1,30 @@
 package com.squabbi.iitk.viewmodel;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.squabbi.iitk.adapter.PortalAdapter;
+import com.squabbi.iitk.db.FirebaseQueryLiveData;
+import com.squabbi.iitk.util.Constants;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 public class PortalListViewModel extends ViewModel {
 
-    public PortalAdapter mAdapter;
+    private static final FirebaseAuth FIREBASE_AUTH = FirebaseAuth.getInstance();
+    private static final CollectionReference AGENT_PORTAL_REF = FirebaseFirestore.getInstance()
+            .collection(Constants.COLLECTION_AGENTS)
+            .document(FIREBASE_AUTH.getUid())
+            .collection(Constants.COLLECTION_PORTALS);
+
+    private final FirebaseQueryLiveData mLiveData = new FirebaseQueryLiveData(AGENT_PORTAL_REF);
+
+    @NonNull
+    public LiveData<QuerySnapshot> getQuerySnapshotLiveData() {
+        return mLiveData;
+    }
 }
