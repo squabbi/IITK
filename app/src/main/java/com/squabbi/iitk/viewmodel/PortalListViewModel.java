01e933496176ab658a6ab17f0d9bdebc5927c877
squabbi <squabbi@users.noreply.github.com>
2018-09-26 13:50:10 +1000

    Removed unnecessary LiveData instantiations and imports

--- app/src/main/java/com/squabbi/iitk/viewmodel/PortalListViewModel.java
+++ app/src/main/java/com/squabbi/iitk/viewmodel/PortalListViewModel.java
@@ -2,25 +2,14 @@ package com.squabbi.iitk.viewmodel;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.squabbi.iitk.adapter.PortalAdapter;
-import com.squabbi.iitk.db.FirebaseQueryLiveData;
-import com.squabbi.iitk.model.Portal;
+import com.squabbi.iitk.db.FirebaseDocChangeLiveData;
 import com.squabbi.iitk.util.Constants;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.Nullable;
-
-import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class PortalListViewModel extends ViewModel {
@@ -32,9 +21,10 @@ public class PortalListViewModel extends ViewModel {
             .document(FIREBASE_AUTH.getUid())
             .collection(Constants.COLLECTION_PORTALS);
 
-    private FirebaseQueryLiveData mLiveData = new FirebaseQueryLiveData(AGENT_PORTAL_REF);
+    private FirebaseDocChangeLiveData mFirebaseDocChangeLiveData = new FirebaseDocChangeLiveData(AGENT_PORTAL_REF);
 
-    public LiveData<List<Portal>> getPortalLiveData() {
-        return mLiveData;
+    public LiveData<List<DocumentChange>> getDocChangesLiveData() {
+
+        return mFirebaseDocChangeLiveData;
     }
 }
