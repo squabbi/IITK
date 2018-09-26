e419ac42cc073fa24f0460d61926a5cd6fd9b3b2
squabbi <squabbi@users.noreply.github.com>
2018-09-26 12:36:40 +1000

    Converted to Portal List LiveData #25

--- app/src/main/java/com/squabbi/iitk/viewmodel/PortalListViewModel.java
+++ app/src/main/java/com/squabbi/iitk/viewmodel/PortalListViewModel.java
@@ -1,6 +1,9 @@
 package com.squabbi.iitk.viewmodel;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -10,7 +13,10 @@ import com.squabbi.iitk.db.FirebaseQueryLiveData;
 import com.squabbi.iitk.model.Portal;
 import com.squabbi.iitk.util.Constants;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.annotation.Nullable;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
@@ -28,8 +34,7 @@ public class PortalListViewModel extends ViewModel {
 
     private FirebaseQueryLiveData mLiveData = new FirebaseQueryLiveData(AGENT_PORTAL_REF);
 
-    @NonNull
-    public LiveData<QuerySnapshot> getQuerySnapshotLiveData() {
+    public LiveData<List<Portal>> getPortalLiveData() {
         return mLiveData;
     }
 }
