64cab154b09bf6f9ea458962db7adb555b47582a
squabbi <squabbi@users.noreply.github.com>
2018-09-26 20:06:23 +1000

    Clean up of ViewModel

--- app/src/main/java/com/squabbi/iitk/viewmodel/PortalListViewModel.java
+++ app/src/main/java/com/squabbi/iitk/viewmodel/PortalListViewModel.java
@@ -1,18 +1,26 @@
 package com.squabbi.iitk.viewmodel;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.squabbi.iitk.db.FirebaseDocChangeLiveData;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.squabbi.iitk.util.Constants;
 
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class PortalListViewModel extends ViewModel {
+
+    private static final String TAG = "PortalListViewModel";
 
     private static final FirebaseAuth FIREBASE_AUTH = FirebaseAuth.getInstance();
 
@@ -21,10 +29,39 @@ public class PortalListViewModel extends ViewModel {
             .document(FIREBASE_AUTH.getUid())
             .collection(Constants.COLLECTION_PORTALS);
 
-    private FirebaseDocChangeLiveData mFirebaseDocChangeLiveData = new FirebaseDocChangeLiveData(AGENT_PORTAL_REF);
+    private CollectionReference PortalCollection = FirebaseFirestore.getInstance()
+            .collection(Constants.COLLECTION_AGENTS).document(FIREBASE_AUTH.getUid())
+            .collection(Constants.COLLECTION_PORTALS);
 
-    public LiveData<List<DocumentChange>> getDocChangesLiveData() {
+    private MutableLiveData<List<DocumentSnapshot>> mListMutableLiveData;
 
-        return mFirebaseDocChangeLiveData;
+    public LiveData<List<DocumentSnapshot>> getPortalDocumentChange() {
+        if (mListMutableLiveData == null) {
+            // Create a new list
+            mListMutableLiveData = new MutableLiveData<>();
+            loadPortalsFromFirestore();
+        }
+
+        return mListMutableLiveData;
+    }
+
+    private void loadPortalsFromFirestore() {
+        // TODO: Move into other class
+//        AGENT_PORTAL_REF.addSnapshotListener(new EventListener<QuerySnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable QuerySnapshot documentSnapshots, @Nullable FirebaseFirestoreException e) {
+//                if (e != null) {
+//                    Log.w(TAG, "FirebaseFirestore e::" + e.toString());
+//                }
+//                mListMutableLiveData.postValue(documentSnapshots.getDocumentChanges());
+//            }
+//        });
+
+        PortalCollection.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot documentSnapshots, @Nullable FirebaseFirestoreException e) {
+                mListMutableLiveData.postValue(documentSnapshots.getDocuments());
+            }
+        });
     }
 }
