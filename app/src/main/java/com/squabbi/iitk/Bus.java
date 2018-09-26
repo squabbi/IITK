1c8f83f43060a1ac4746dfddac29d4fa405e8e1e
squabbi <squabbi@users.noreply.github.com>
2018-09-26 10:56:02 +1000

    Removed EventBus singleton

--- app/src/main/java/com/squabbi/iitk/Bus.java
+++
@@ -1,9 +0,0 @@
-package com.squabbi.iitk;
-
-import org.greenrobot.eventbus.EventBus;
-
-public class Bus {
-    private static EventBus sBus = new EventBus();
-
-    public static EventBus getInstance() { return sBus; }
-}
