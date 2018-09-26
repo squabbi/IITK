7697c9522bbe9a228406611f04ec2f6bfe04270b
squabbi <squabbi@users.noreply.github.com>
2018-09-26 22:54:14 +1000

    Basic Add Portal ViewModel

--- app/src/main/java/com/squabbi/iitk/viewmodel/NewPortalViewModel.java
+++ app/src/main/java/com/squabbi/iitk/viewmodel/NewPortalViewModel.java
@@ -1,7 +1,22 @@
 package com.squabbi.iitk.viewmodel;
 
-import androidx.databinding.BaseObservable;
+import com.google.android.gms.location.places.Place;
+import com.squabbi.iitk.model.Portal;
+import com.squabbi.iitk.util.FirebaseRepository;
 
-public class NewPortalViewModel extends BaseObservable {
+import androidx.lifecycle.ViewModel;
 
+public class NewPortalViewModel extends ViewModel {
+
+    private static final String TAG = "NewPortalViewModel";
+
+    private FirebaseRepository mFirebaseRepository = FirebaseRepository.getInstance();
+
+    public boolean addPortal(String name, Place place, String friendlyLocation,
+                             String notes, String colour) {
+
+        // Create new Portal object
+        Portal portal = new Portal(name, place, friendlyLocation, notes, colour);
+        return mFirebaseRepository.addPortal(portal);
+    }
 }
