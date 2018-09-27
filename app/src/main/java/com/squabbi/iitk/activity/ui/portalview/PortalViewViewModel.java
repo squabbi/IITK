7e743a447029d26c76399af519ead341f0078592
squabbi <squabbi@users.noreply.github.com>
2018-09-27 19:32:52 +1000

    Portal View viewmodel

---
+++ app/src/main/java/com/squabbi/iitk/activity/ui/portalview/PortalViewViewModel.java
@@ -0,0 +1,7 @@
+package com.squabbi.iitk.activity.ui.portalview;
+
+import androidx.lifecycle.ViewModel;
+
+public class PortalViewViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
