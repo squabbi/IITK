e828e9a77b669a293e5fb7cff8964002136297bf
squabbi <squabbi@users.noreply.github.com>
2018-09-27 14:53:05 +1000

    Simplified nav drawer toggle

--- app/src/main/java/com/squabbi/iitk/activity/MainActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/MainActivity.java
@@ -3,7 +3,6 @@ package com.squabbi.iitk.activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.navigation.NavigationView;
@@ -16,15 +15,13 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.squabbi.iitk.R;
+import com.squabbi.iitk.adapter.PortalAdapter;
 import com.squabbi.iitk.fragment.InventoryFragment;
 import com.squabbi.iitk.fragment.PortalListFragment;
 import com.squabbi.iitk.viewmodel.MainActivityViewModel;
@@ -47,7 +44,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @BindView(R.id.drawer_layout) DrawerLayout mDrawerLayout;
     @BindView(R.id.nav_view) NavigationView mNavigationView;
     @BindView(R.id.main_activity_appbar) BottomAppBar mBottomAppBar;
-    private ActionBarDrawerToggle mDrawerToggle;
     private MainActivityViewModel mMainActivityViewModel;
 
     // Fragments
@@ -86,8 +82,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
 
-        mDrawerLayout.addDrawerListener(mDrawerToggle);
-
         // assign fragments
         mPortalListFragment = new PortalListFragment();
         mInventoryFragment = new InventoryFragment();
@@ -112,13 +106,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 startActivity(new Intent(this, NewInventoryActivity.class));
                 break;
         }
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Pass any configuration changes to the drawerToggle
-        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -147,8 +134,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.menu_open_intel:
                 openIntelMap(0);
                 break;
+            case android.R.id.home:
+                // Open navigation bar
+                toggleNavigationDrawer();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void toggleNavigationDrawer() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else mDrawerLayout.openDrawer(GravityCompat.START);
     }
 
     private void openIntelMap(int intelType) {
