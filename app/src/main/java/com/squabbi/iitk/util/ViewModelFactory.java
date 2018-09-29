eed1d3469c0189a1543b5f745a597c01612c0751
squabbi <squabbi@users.noreply.github.com>
2018-09-29 11:25:57 +1000

    Completed ViewModel factory

--- app/src/main/java/com/squabbi/iitk/util/ViewModelFactory.java
+++ app/src/main/java/com/squabbi/iitk/util/ViewModelFactory.java
@@ -1,6 +1,29 @@
 package com.squabbi.iitk.util;
 
-import ViewModelProvider.NewInstanceFactory;
+import com.squabbi.iitk.activity.ui.portalview.PortalViewViewModel;
 
-public class ViewModelFactory extends NewInstanceFactory {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class ViewModelFactory extends ViewModelProvider.NewInstanceFactory {
+
+    private Object[] mParams;
+
+    public ViewModelFactory(Object... params) {
+
+        super();
+        this.mParams = params;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+
+        if (modelClass == PortalViewViewModel.class) {
+            return (T) new PortalViewViewModel((String) mParams[0]);
+        } else {
+            return super.create(modelClass);
+        }
+    }
 }
