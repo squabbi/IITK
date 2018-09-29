80beba592bc775f913f7a89e9903038825c12936
squabbi <squabbi@users.noreply.github.com>
2018-09-29 17:35:00 +1000

    Added UI and EditText validations

--- app/src/main/java/com/squabbi/iitk/activity/ui/newportal/NewPortalActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/newportal/NewPortalActivity.java
@@ -9,34 +9,57 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.Gravity;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnTextChanged;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.kunzisoft.androidclearchroma.listener.OnColorSelectedListener;
 import com.squabbi.iitk.R;
+import com.squabbi.iitk.databinding.ActivityNewPortalBinding;
 
 public class NewPortalActivity extends AppCompatActivity implements OnColorSelectedListener {
 
-    @BindView(R.id.new_portal_toolbar) Toolbar mToolbar;
-    @BindView(R.id.portal_name_et) EditText mPortalNameEt;
-    @BindView(R.id.portal_notes_et) EditText mPortalNotesEt;
-    @BindView(R.id.portal_location_et) EditText mPortalLocationEt;
-    @BindView(R.id.colour_bar_view) View mColourBarView;
+    @BindView(R.id.new_portal_toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.portal_name_inputlayout)
+    TextInputLayout mNameInputLayout;
+
+    @BindView(R.id.portal_name_et)
+    EditText mPortalNameEt;
+
+    @OnTextChanged(value = R.id.portal_name_et, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    void nameTextEntered(Editable editable) {
+        mNameInputLayout.setErrorEnabled(false);
+    }
+
+    @BindView(R.id.portal_notes_et)
+    EditText mPortalNotesEt;
+
+    @BindView(R.id.portal_friendly_location_et)
+    EditText mPortalFriendlyLocation;
+
+    @BindView(R.id.colour_bar_view)
+    View mColourBarView;
+
+    @BindView(R.id.new_portal_fab)
+    FloatingActionButton mFab;
 
     private NewPortalViewModel mViewModel;
     private Place mPlace;
@@ -48,12 +71,18 @@ public class NewPortalActivity extends AppCompatActivity implements OnColorSelec
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_portal);
+
+        mViewModel = ViewModelProviders.of(this).get(NewPortalViewModel.class);
+
+        // Bind the viewmodel to view.
+        ActivityNewPortalBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_new_portal);
+        binding.setViewmodel(mViewModel);
+        binding.setLifecycleOwner(this);
+
         ButterKnife.bind(this);
 
-        // Set ViewModel
-        mViewModel = ViewModelProviders.of(this).get(NewPortalViewModel.class);
         // Observe changes to ColourPicker LiveData
         mViewModel.getColourLiveData().observe(this, new Observer<Integer>() {
             @Override
@@ -71,8 +100,7 @@ public class NewPortalActivity extends AppCompatActivity implements OnColorSelec
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_outline_close_24px);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+       mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mViewModel.getChromaDialogBuilder()
@@ -108,20 +136,13 @@ public class NewPortalActivity extends AppCompatActivity implements OnColorSelec
             case R.id.menu_done:
                 // Close the keyboard and submit strings to ViewModel.
                 closeKeyboard();
-                // Add portal
-                if (!addPortal()) {
-                    // If there was an issue adding the portal. Don't leave the screen.
-                    Toast toast = Toast.makeText(this, R.string.toast_addPortal_failed, Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.TOP, 0,0);
-                    toast.show();
-                    break;
-                } else {
-                    // Finish activity
+                // Validate entries
+                if (validateText()) {
+                    // Add portal and exit activity
+                    addPortal();
                     finish();
                 }
-                return true;
         }
-        // Invoke super for all other items.
         return super.onOptionsItemSelected(item);
     }
 
@@ -133,16 +154,27 @@ public class NewPortalActivity extends AppCompatActivity implements OnColorSelec
         }
     }
 
-    private boolean addPortal() {
+    private boolean validateText() {
+        // Check for empty name
+        if (isEmpty(mPortalNameEt)) {
+            mNameInputLayout.setError("Portal name cannot be empty");
+            mNameInputLayout.setErrorEnabled(true);
+            return false;
+        }
+
+        return false;
+    }
+
+    private void addPortal() {
         // Complete the action and add the Portal to the Database.
-        // Get strings
+        // Get strings from textviews
         String name = mPortalNameEt.getText().toString();
         String notes = mPortalNotesEt.getText().toString();
         Place place = mPlace;
-        String friendlyLocation = mPortalLocationEt.getText().toString();
+        String friendlyLocation = mPortalFriendlyLocation.getText().toString();
         Integer colour = mColour;
 
-        return mViewModel.addPortal(name, place, friendlyLocation, notes, colour);
+        mViewModel.addPortal(name, place, friendlyLocation, notes, colour);
     }
 
     @Override
@@ -152,11 +184,17 @@ public class NewPortalActivity extends AppCompatActivity implements OnColorSelec
         if (requestCode == PLACE_PICKER_REQUEST) {
             if (resultCode == RESULT_OK) {
                 mPlace = PlacePicker.getPlace(this, data);
-                EditText et = findViewById(R.id.portal_location_et);
-                // Display friendly Place name
-                et.setText(String.valueOf(mPlace.getName()));
+                // Display friendly Place name only if it is empty
+                if (isEmpty(mPortalFriendlyLocation)) {
+                    mPortalFriendlyLocation.setText(String.valueOf(mPlace.getName()));
+                }
+                mViewModel.setPortalLocationLiveData(mPlace.getLatLng().toString());
             }
         }
+    }
+
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
     }
 
     @Override
