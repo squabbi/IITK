20a650f218940a054fcedf6119f351f3224109e2
squabbi <squabbi@users.noreply.github.com>
2018-09-29 11:43:09 +1000

    Instantiate PortalView ViewModel with documentpath

--- app/src/main/java/com/squabbi/iitk/activity/PortalViewActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/PortalViewActivity.java
@@ -1,18 +1,30 @@
 package com.squabbi.iitk.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
 import com.squabbi.iitk.R;
+import com.squabbi.iitk.activity.ui.mainlistview.PortalListFragment;
 import com.squabbi.iitk.activity.ui.portalview.PortalViewFragment;
+import com.squabbi.iitk.activity.ui.portalview.PortalViewViewModel;
+import com.squabbi.iitk.util.ViewModelFactory;
 
 public class PortalViewActivity extends AppCompatActivity {
+
+    PortalViewViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.portal_view_activity);
+        setContentView(R.layout.activity_portal_view);
+
+        // Get string from intent
+        mViewModel = ViewModelProviders.of(this,
+                new ViewModelFactory(getIntent().getStringExtra(PortalListFragment.PORTAL_ID_KEY)))
+                .get(PortalViewViewModel.class);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, PortalViewFragment.newInstance())
