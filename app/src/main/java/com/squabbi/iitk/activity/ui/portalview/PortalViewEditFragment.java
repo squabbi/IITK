fbbbd50a8dd3de0f36e3458f2487ae35fe191fe5
squabbi <squabbi@users.noreply.github.com>
2018-09-30 09:33:33 +1000

    Added simple edit fragment

--- app/src/main/java/com/squabbi/iitk/activity/ui/portalview/PortalViewEditFragment.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/portalview/PortalViewEditFragment.java
@@ -1,6 +1,30 @@
 package com.squabbi.iitk.activity.ui.portalview;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 public class PortalViewEditFragment extends Fragment {
+
+    public static PortalViewEditFragment newInstance() { return new PortalViewEditFragment(); }
+
+    private PortalViewViewModel mViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = ViewModelProviders.of(getActivity()).get(PortalViewViewModel.class);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
