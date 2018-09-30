ab101a8046305c4d59648ab38c730a58557ea944
squabbi <squabbi@users.noreply.github.com>
2018-09-30 14:41:04 +1000

    Added bindings and layout inflation for ViewEdit

--- app/src/main/java/com/squabbi/iitk/activity/ui/portalview/PortalViewEditFragment.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/portalview/PortalViewEditFragment.java
@@ -5,10 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.squabbi.iitk.databinding.FragmentPortalViewEditBinding;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import butterknife.ButterKnife;
 
 public class PortalViewEditFragment extends Fragment {
 
@@ -24,7 +27,15 @@ public class PortalViewEditFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        FragmentPortalViewEditBinding binding = FragmentPortalViewEditBinding
+                .inflate(inflater, container, false);
+        ButterKnife.bind(this, binding.getRoot());
+
+        binding.setViewmodel(mViewModel);
+        binding.setLifecycleOwner(this);
+        return binding.getRoot();
     }
 }
