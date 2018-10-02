44841ec32f15bfdcffa87afba67686dc49ab22ad
squabbi <squabbi@users.noreply.github.com>
2018-10-02 17:50:58 +1000

    Minor refactor

--- app/src/main/java/com/squabbi/iitk/activity/ui/mainlistview/InventoryListFragment.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/mainlistview/InventoryListFragment.java
@@ -20,19 +20,13 @@ public class InventoryListFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_inventory_list, container, false);
-
         TextView text = view.findViewById(R.id.inv_text);
-
-
-
-       // text.setText(query.get().);
 
         return view;
     }
