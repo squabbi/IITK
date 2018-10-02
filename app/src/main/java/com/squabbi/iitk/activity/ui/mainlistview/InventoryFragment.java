aa67a651dced522ea4e1b7204af608029c08214a
squabbi <squabbi@users.noreply.github.com>
2018-10-02 16:52:59 +1000

    Removed Firebase mentions

--- app/src/main/java/com/squabbi/iitk/activity/ui/mainlistview/InventoryFragment.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/mainlistview/InventoryFragment.java
@@ -6,23 +6,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
+import android.widget.TextView
 import com.squabbi.iitk.R;
-import com.squabbi.iitk.util.Constants;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InventoryFragment extends Fragment {
-
-    private FirebaseAuth mAuth;
-    private FirebaseFirestore mFirestore;
-
 
     public InventoryFragment() {
         // Required empty public constructor
@@ -32,12 +23,6 @@ public class InventoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-        mAuth = FirebaseAuth.getInstance();
-        mFirestore = FirebaseFirestore.getInstance();
-        //Query query = mFirestore.collection(Constants.COLLECTION_AGENTS).document(mAuth.getUid()).collection(Constants.COLLECTION_PORTALS);
-
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_inventory_list, container, false);
