c73d5a5688c1d7e94c352fc09aa09017d5acf0fe
squabbi <squabbi@users.noreply.github.com>
2018-10-03 19:23:17 +1000

    Added basic getter and setter for selected capsule array

--- app/src/main/java/com/squabbi/iitk/activity/ui/newinventory/NewInventoryViewModel.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/newinventory/NewInventoryViewModel.java
@@ -1,8 +1,21 @@
 package com.squabbi.iitk.activity.ui.newinventory;
 
+import android.graphics.Color;
+
 import androidx.lifecycle.ViewModel;
 
 public class NewInventoryViewModel extends ViewModel {
 
-    private boolean[] selectedCapsules = new boolean[6];
+    private boolean[] mSelectedCapsules = new boolean[6];
+    private Color mColor;
+
+    public void setSelectedCapsule(int capsule) {
+
+        mSelectedCapsules[capsule] = true;
+    }
+
+    public boolean getSelectedCapsule(int capsule) {
+        
+        return mSelectedCapsules[capsule];
+    }
 }
