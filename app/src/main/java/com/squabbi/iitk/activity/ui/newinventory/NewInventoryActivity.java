5e8b0160343f8849c5142554ab441ec8528ddb6d
squabbi <squabbi@users.noreply.github.com>
2018-10-03 18:39:26 +1000

    Registered ViewModel

--- app/src/main/java/com/squabbi/iitk/activity/ui/newinventory/NewInventoryActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/newinventory/NewInventoryActivity.java
@@ -1,6 +1,7 @@
 package com.squabbi.iitk.activity.ui.newinventory;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -14,11 +15,16 @@ public class NewInventoryActivity extends AppCompatActivity {
 
     @BindView(R.id.lockers_gridview) GridView mLockerGridView;
 
+    private NewInventoryViewModel mViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_inventory);
         ButterKnife.bind(this);
+
+        // Register ViewModel
+        mViewModel = ViewModelProviders.of(this).get(NewInventoryViewModel.class);
 
         mLockerGridView.setAdapter(new KeyLockerAdapter(this));
     }
