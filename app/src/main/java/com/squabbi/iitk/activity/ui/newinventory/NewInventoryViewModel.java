b594e4ade26618636168b473b43cdf0fb486c2a9
squabbi <squabbi@users.noreply.github.com>
2018-10-04 17:07:29 +1000

    Added LiveData for boolean array

--- app/src/main/java/com/squabbi/iitk/activity/ui/newinventory/NewInventoryViewModel.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/newinventory/NewInventoryViewModel.java
@@ -2,20 +2,22 @@ package com.squabbi.iitk.activity.ui.newinventory;
 
 import android.graphics.Color;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class NewInventoryViewModel extends ViewModel {
 
+    private MutableLiveData<boolean[]> mSelectedCapsulesLiveData = new MutableLiveData<>();
     private boolean[] mSelectedCapsules = new boolean[6];
     private Color mColor;
 
-    public void setSelectedCapsule(int capsule) {
+    public void setSelectedCapsule(int capsule, boolean state) {
 
-        mSelectedCapsules[capsule] = true;
+        mSelectedCapsules[capsule] = state;
     }
 
     public boolean getSelectedCapsule(int capsule) {
-        
+
         return mSelectedCapsules[capsule];
     }
 }
