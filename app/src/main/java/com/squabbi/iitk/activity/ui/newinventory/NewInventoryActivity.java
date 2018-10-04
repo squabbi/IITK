e042ba609b2046459387003a5307f0c2a22860ca
squabbi <squabbi@users.noreply.github.com>
2018-10-04 23:12:31 +1000

    Added OnClick and conditional formatting for selected lockers

--- app/src/main/java/com/squabbi/iitk/activity/ui/newinventory/NewInventoryActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/newinventory/NewInventoryActivity.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProviders;
 import butterknife.BindView;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -28,6 +29,34 @@ public class NewInventoryActivity extends AppCompatActivity {
             R.id.new_inv_anniversary_locker_imageview })
     List<ImageView> mLockerImageViews;
 
+    // Set onClickListener for Locker ImageViews
+    @OnClick({ R.id.new_inv_green_locker_imageview, R.id.new_inv_blue_locker_imageview,
+            R.id.new_inv_white_locker_imageview, R.id.new_inv_red_locker_imageview, R.id.new_inv_yellow_locker_imageview,
+            R.id.new_inv_anniversary_locker_imageview })
+    public void selectLocker(ImageView imageView) {
+
+        switch (imageView.getId()) {
+            case R.id.new_inv_green_locker_imageview:
+                mViewModel.setSelectedCapsule(0, !mViewModel.getSelectedCapsule(0));
+                break;
+            case R.id.new_inv_blue_locker_imageview:
+                mViewModel.setSelectedCapsule(1, !mViewModel.getSelectedCapsule(1));
+                break;
+            case R.id.new_inv_white_locker_imageview:
+                mViewModel.setSelectedCapsule(2, !mViewModel.getSelectedCapsule(2));
+                break;
+            case R.id.new_inv_red_locker_imageview:
+                mViewModel.setSelectedCapsule(3, !mViewModel.getSelectedCapsule(3));
+                break;
+            case R.id.new_inv_yellow_locker_imageview:
+                mViewModel.setSelectedCapsule(4, !mViewModel.getSelectedCapsule(4));
+                break;
+            case R.id.new_inv_anniversary_locker_imageview:
+                mViewModel.setSelectedCapsule(5, !mViewModel.getSelectedCapsule(5));
+                break;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +66,7 @@ public class NewInventoryActivity extends AppCompatActivity {
         // Register ViewModel
         mViewModel = ViewModelProviders.of(this).get(NewInventoryViewModel.class);
 
-        // Observe livedata for checkboxes
+        // Observe LiveData for checkboxes
         mViewModel.getSelectedCapsules().observe(this, new Observer<boolean[]>() {
             @Override
             public void onChanged(boolean[] booleans) {
@@ -45,11 +74,12 @@ public class NewInventoryActivity extends AppCompatActivity {
                 for (int i = 0; i < Constants.LockerTypes.length; i++) {
                     if (booleans[i]) {
                         mLockerImageViews.get(i).setBackground(new ColorDrawable(Color.RED));
+                    } else {
+                        // Unhighlight the capsule
+                        mLockerImageViews.get(i).setBackground(new ColorDrawable(Color.TRANSPARENT));
                     }
                 }
             }
         });
-
-        mViewModel.setSelectedCapsule(2, true);
     }
 }
