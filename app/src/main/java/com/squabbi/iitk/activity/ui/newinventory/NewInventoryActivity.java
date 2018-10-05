997cdc32a9fce9e2469b0d378e05610a9c39669e
squabbi <squabbi@users.noreply.github.com>
2018-10-05 13:50:42 +1000

    #33 prepare removal of activity

--- app/src/main/java/com/squabbi/iitk/activity/ui/newinventory/NewInventoryActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/newinventory/NewInventoryActivity.java
@@ -1,20 +1,29 @@
 package com.squabbi.iitk.activity.ui.newinventory;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import butterknife.BindView;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-import android.graphics.Color;
+import android.content.Context;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.squabbi.iitk.R;
+import com.squabbi.iitk.databinding.ActivityNewInventoryBinding;
 import com.squabbi.iitk.util.Constants;
 
 import java.util.List;
@@ -22,6 +31,18 @@ import java.util.List;
 public class NewInventoryActivity extends AppCompatActivity {
 
     private NewInventoryViewModel mViewModel;
+    private Integer mColour;
+
+    private static final String TAG_CHROMA_DIALOG = "TAG_CHROMA_DIALOG";
+
+    @BindView(R.id.new_inventory_toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.new_inventory_colourbar)
+    View mColourBar;
+
+    @BindView(R.id.new_inventory_name_edittext)
+    EditText mNameEt;
 
     // Array of Locker ImageViews
     @BindViews({ R.id.new_inv_green_locker_imageview, R.id.new_inv_blue_locker_imageview,
@@ -57,6 +78,113 @@ public class NewInventoryActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Register ViewModel
+        mViewModel = ViewModelProviders.of(this).get(NewInventoryViewModel.class);
+
+        // Bind the ViewModel to XML
+        ActivityNewInventoryBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_new_inventory);
+        binding.setViewModel(mViewModel);
+        binding.setLifecycleOwner(this);
+
+        ButterKnife.bind(this);
+
+        // Set initial status of Lockers
+        for (ImageView v : mLockerImageViews) {
+            setUnselected(v);
+        }
+
+        // Observe LiveData for selcted lockers
+        mViewModel.getSelectedCapsules().observe(this, new Observer<boolean[]>() {
+            @Override
+            public void onChanged(boolean[] booleans) {
+                // Set the checked status depending on array
+                for (int i = 0; i < Constants.LockerTypes.length; i++) {
+                    if (booleans[i]) {
+                        setSelected(mLockerImageViews.get(i));
+                    } else {
+                        // Unhighlight the capsule
+                        setUnselected(mLockerImageViews.get(i));
+                    }
+                }
+            }
+        });
+
+        // Observe changes to colour bar live data
+        mViewModel.getColourLiveData().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                mColour = integer;
+                ColorDrawable colorDrawable = new ColorDrawable(integer);
+                mColourBar.setBackground(colorDrawable);
+            }
+        });
+
+        setSupportActionBar(mToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar and
+        // enable the close button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_outline_close_24px);
+
+        // TODO: Set fab for colour picker
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_done_add, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_done:
+                // Close the keyboard and submit strings to ViewModel.
+                closeKeyboard();
+                // Validate entries
+                if (validateText()) {
+                    // Add portal and exit activity
+                    addInventory();
+                    finish();
+                }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager iMm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            iMm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    private boolean validateText() {
+        boolean passingStatus = true;
+
+        // Check for empty name
+        if (isEmpty(mNameEt)) {
+            //mNameInputLayout.setError(getString(R.string.inputlayout_error_emptyname));
+            //mNameInputLayout.setErrorEnabled(true);
+            passingStatus = false;
+        }
+
+        return passingStatus;
+    }
+
+    private void addInventory() {
+
+        // Complete the action and add the Portal to the Database.
+        // Get strings from textviews
+        String name = mNameEt.getText().toString();
+
+        mViewModel.addInventory(name, "Sample description");
+    }
+
     private static void setUnselected(ImageView imageView) {
 
         ColorMatrix matrix = new ColorMatrix();
@@ -73,34 +201,7 @@ public class NewInventoryActivity extends AppCompatActivity {
         imageView.setImageAlpha(255);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_inventory);
-        ButterKnife.bind(this);
-
-        // Register ViewModel
-        mViewModel = ViewModelProviders.of(this).get(NewInventoryViewModel.class);
-
-        // Set initial status of Lockers
-        for (ImageView v : mLockerImageViews) {
-            setUnselected(v);
-        }
-
-        // Observe LiveData for checkboxes
-        mViewModel.getSelectedCapsules().observe(this, new Observer<boolean[]>() {
-            @Override
-            public void onChanged(boolean[] booleans) {
-                // Set the checked status depending on array
-                for (int i = 0; i < Constants.LockerTypes.length; i++) {
-                    if (booleans[i]) {
-                        setSelected(mLockerImageViews.get(i));
-                    } else {
-                        // Unhighlight the capsule
-                        setUnselected(mLockerImageViews.get(i));
-                    }
-                }
-            }
-        });
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
     }
 }
--- app/src/main/java/com/squabbi/iitk/activity/ui/newinventory/NewInventoryViewModel.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/newinventory/NewInventoryViewModel.java
@@ -2,15 +2,61 @@ package com.squabbi.iitk.activity.ui.newinventory;
 
 import android.graphics.Color;
 
+import com.kunzisoft.androidclearchroma.ChromaDialog;
+import com.kunzisoft.androidclearchroma.IndicatorMode;
+import com.kunzisoft.androidclearchroma.colormode.ColorMode;
+import com.squabbi.iitk.model.Capsule;
+import com.squabbi.iitk.model.Inventory;
+import com.squabbi.iitk.model.Item;
+import com.squabbi.iitk.model.KeyLocker;
+import com.squabbi.iitk.util.Constants;
+import com.squabbi.iitk.util.FirebaseRepository;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class NewInventoryViewModel extends ViewModel {
 
+    private FirebaseRepository mFirebaseRepository = FirebaseRepository.getInstance();
     private MutableLiveData<boolean[]> mSelectedCapsulesLiveData = new MutableLiveData<>();
     private boolean[] mSelectedCapsules = new boolean[6];
-    private Color mColor;
+    private MutableLiveData<Integer> mColorLiveData = new MutableLiveData<>();
+
+    public NewInventoryViewModel() {
+
+        // Set default colour
+        setColorLiveData(Color.WHITE);
+    }
+
+    public void addInventory(String name, String description) {
+
+        Constants.LockerType[] lockerTypes = Constants.LockerType.values();
+
+        // Generate items list
+        List<Item> items = new LinkedList<>();
+        for (int i = 0; i < mSelectedCapsules.length; i++) {
+            if (mSelectedCapsules[i]) {
+                items.add(new KeyLocker(lockerTypes[i], String.valueOf(i)));
+            }
+        }
+
+        // Create inventory object
+        Inventory inventory = new Inventory(name, description,
+                getColourLiveData().getValue(), items);
+        mFirebaseRepository.addInventory(inventory);
+    }
+
+    public ChromaDialog.Builder getChromaDialogBuilder() {
+
+        return new ChromaDialog.Builder()
+                .initialColor(Color.GRAY)
+                .colorMode(ColorMode.RGB)
+                .indicatorMode(IndicatorMode.HEX);
+    }
 
     public void setSelectedCapsule(int capsule, boolean state) {
 
@@ -20,12 +66,16 @@ public class NewInventoryViewModel extends ViewModel {
     }
 
     public boolean getSelectedCapsule(int capsule) {
-
         return mSelectedCapsules[capsule];
     }
 
     public LiveData<boolean[]> getSelectedCapsules() {
-
         return mSelectedCapsulesLiveData;
     }
+
+    public LiveData<Integer> getColourLiveData() { return mColorLiveData; }
+
+    public void setColorLiveData(Integer colour) { mColorLiveData.setValue(colour); }
+
+
 }
