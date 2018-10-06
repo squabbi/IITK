2bbcde5e03224d5b30c815b236add0631ee8121d
squabbi <squabbi@users.noreply.github.com>
2018-10-06 17:03:33 +1000

    Added base HoverMenuService

--- app/src/main/java/com/squabbi/iitk/service/MasterHoverMenuService.java
+++ app/src/main/java/com/squabbi/iitk/service/MasterHoverMenuService.java
@@ -1,6 +1,34 @@
 package com.squabbi.iitk.service;
 
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import io.mattcarroll.hover.HoverMenu;
+import io.mattcarroll.hover.HoverView;
 import io.mattcarroll.hover.window.HoverMenuService;
 
 public class MasterHoverMenuService extends HoverMenuService {
+
+    public static void showFloatingMenu(Context context) {
+
+        context.startService(new Intent(context, MasterHoverMenuService.class));
+    }
+
+    // TODO: May have to implement EventBus in onCreate and onDestroy
+
+
+    @Override
+    protected void onHoverMenuLaunched(@NonNull Intent intent, @NonNull HoverView hoverView) {
+//        super.onHoverMenuLaunched(intent, hoverView);
+        hoverView.setMenu(createHoverMenu());
+        hoverView.collapse();
+    }
+
+    private HoverMenu createHoverMenu() {
+
+        try {
+            return new HoverMenuFactory()
+        }
+    }
 }
