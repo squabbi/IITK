c6611eb58a30673495c434ebdad35877ce17ec72
squabbi <squabbi@users.noreply.github.com>
2018-10-06 17:28:19 +1000

    Added base factory

--- app/src/main/java/com/squabbi/iitk/hover/HoverMenuFactory.java
+++ app/src/main/java/com/squabbi/iitk/hover/HoverMenuFactory.java
@@ -1,4 +1,20 @@
 package com.squabbi.iitk.hover;
 
+import android.content.Context;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import androidx.annotation.NonNull;
+import io.mattcarroll.hover.Content;
+
 public class HoverMenuFactory {
+
+    public MasterHoverMenu createMenus(@NonNull Context context) throws IOException {
+
+        // Create a map with keys and respective menus
+        Map<String, Content> menuMap = new LinkedHashMap<>();
+        menuMap.put(MasterHoverMenu)
+    }
 }
