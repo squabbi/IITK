ec612ca5737f7d4dfa1f3ad4534e0f8ce43e23a5
squabbi <squabbi@users.noreply.github.com>
2018-10-09 23:23:10 +1100

    Added Draw over other apps permissions

--- app/src/main/java/com/squabbi/iitk/activity/SettingsActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/SettingsActivity.java
@@ -1,8 +1,10 @@
 package com.squabbi.iitk.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.mattcarroll.hover.overlay.OverlayPermission;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +15,9 @@ import com.squabbi.iitk.service.MyHoverMenuService;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private static final Integer REQUEST_CODE_HOVER_PERMISSION = 91;
+    private boolean mPermissionsRequested = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +27,27 @@ public class SettingsActivity extends AppCompatActivity {
 
     @OnClick(R.id.start_hover_button)
     void onClick(View view) {
-        // TODO: Implement hover service start
-        MyHoverMenuService.showFloatingMenu(getApplicationContext());
+        startHoverService();
+    }
+
+    public void startHoverService() {
+
+        // On Android M and above we need to ask the user for permission to display the Hover
+        // menu within the "alert window" layer.  Use OverlayPermission to check for the permission
+        // and to request it.
+        if (!mPermissionsRequested && !OverlayPermission.hasRuntimePermissionToDrawOverlay(this)) {
+            Intent intent = OverlayPermission.createIntentToRequestOverlayPermission(this);
+            startActivityForResult(intent, REQUEST_CODE_HOVER_PERMISSION);
+        } else { MyHoverMenuService.showFloatingMenu(this); }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (REQUEST_CODE_HOVER_PERMISSION == requestCode) {
+            mPermissionsRequested = true;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
