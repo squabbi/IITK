b99378c68efb335eb86502fe3c909275a21f0f03
squabbi <squabbi@users.noreply.github.com>
2018-10-09 14:50:55 +1100

    Added onClick bind for Hover Start button

--- app/src/main/java/com/squabbi/iitk/activity/SettingsActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/SettingsActivity.java
@@ -1,7 +1,12 @@
 package com.squabbi.iitk.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.squabbi.iitk.R;
 
@@ -11,5 +16,11 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.start_hover_button)
+    void onClick(View view) {
+        // TODO: Implement hover service start
     }
 }
