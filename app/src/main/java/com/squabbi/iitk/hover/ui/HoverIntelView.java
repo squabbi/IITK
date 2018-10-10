40aa007d749f826a16b3e2784968c322d95ecd03
squabbi <squabbi@users.noreply.github.com>
2018-10-10 17:40:49 +1100

    Added webclient

--- app/src/main/java/com/squabbi/iitk/hover/ui/HoverIntelView.java
+++ app/src/main/java/com/squabbi/iitk/hover/ui/HoverIntelView.java
@@ -3,6 +3,9 @@ package com.squabbi.iitk.hover.ui;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 
 import com.squabbi.iitk.R;
@@ -11,6 +14,8 @@ import androidx.annotation.NonNull;
 import io.mattcarroll.hover.Content;
 
 public class HoverIntelView extends FrameLayout implements Content {
+
+    private WebView mWebView;
 
     public HoverIntelView(@NonNull Context context) {
         super(context);
@@ -22,7 +27,11 @@ public class HoverIntelView extends FrameLayout implements Content {
     private void init() {
         LayoutInflater.from(getContext()).inflate(R.layout.hoverview_intel, this, true);
 
+        mWebView = findViewById(R.id.hover_intel_webview);
+
         // TODO: Load Ingress intel website
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.loadUrl("https://ingress.com/intel");
     }
 
     @NonNull
