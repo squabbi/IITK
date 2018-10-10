4512143487a8cd4e7140ce91bb3fdce82b6ed2fd
squabbi <squabbi@users.noreply.github.com>
2018-10-10 12:21:48 +1100

    Added base functions and overrides

--- app/src/main/java/com/squabbi/iitk/hover/ui/HoverIntelView.java
+++ app/src/main/java/com/squabbi/iitk/hover/ui/HoverIntelView.java
@@ -1,4 +1,48 @@
 package com.squabbi.iitk.hover.ui;
 
-public class HoverIntelView {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.squabbi.iitk.R;
+
+import androidx.annotation.NonNull;
+import io.mattcarroll.hover.Content;
+
+public class HoverIntelView extends FrameLayout implements Content {
+
+    public HoverIntelView(@NonNull Context context) {
+        super(context);
+
+        // Inflate the layout
+        init();
+    }
+
+    private void init() {
+        LayoutInflater.from(getContext()).inflate(R.layout.hoverview_intel, this, true);
+
+        // TODO: Load Ingress intel website
+    }
+
+    @NonNull
+    @Override
+    public View getView() {
+        return this;
+    }
+
+    @Override
+    public boolean isFullscreen() {
+        return true;
+    }
+
+    @Override
+    public void onShown() {
+
+    }
+
+    @Override
+    public void onHidden() {
+
+    }
 }
