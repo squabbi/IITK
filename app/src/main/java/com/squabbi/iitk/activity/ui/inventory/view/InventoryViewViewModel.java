d15d02b6b5251e3ad891156f5ea7ba6a78d5be0a
squabbi <squabbi@users.noreply.github.com>
2018-10-11 17:38:40 +1100

    WIP implementing viewmodel

--- app/src/main/java/com/squabbi/iitk/activity/ui/inventory/view/InventoryViewViewModel.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/inventory/view/InventoryViewViewModel.java
@@ -1,7 +1,20 @@
 package com.squabbi.iitk.activity.ui.inventory.view;
 
+import com.squabbi.iitk.model.Item;
+import com.squabbi.iitk.util.FirebaseRepository;
+
 import androidx.lifecycle.ViewModel;
 
 public class InventoryViewViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private String mInventoryId;
+
+    private MutableLiveData<>
+
+    private FirebaseRepository mRepository = FirebaseRepository.getInstance();
+
+    public void addItem(Item item) {
+
+        mRepository.addItemToInventory(, )
+    }
 }
