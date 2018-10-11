0e85ce51d2ebef585977652e4c1f34385e393abe
squabbi <squabbi@users.noreply.github.com>
2018-10-11 22:32:42 +1100

    Implemented shell for Firestore interactions

--- app/src/main/java/com/squabbi/iitk/activity/ui/inventory/view/InventoryViewViewModel.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/inventory/view/InventoryViewViewModel.java
@@ -1,20 +1,36 @@
 package com.squabbi.iitk.activity.ui.inventory.view;
 
+import com.google.firebase.firestore.ListenerRegistration;
 import com.squabbi.iitk.model.Item;
 import com.squabbi.iitk.util.FirebaseRepository;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class InventoryViewViewModel extends ViewModel {
 
-    private String mInventoryId;
-
-    private MutableLiveData<>
+    private static final String TAG = "InventoryViewViewModel";
 
     private FirebaseRepository mRepository = FirebaseRepository.getInstance();
+    private String mInventoryId;
+    private MutableLiveData<String> mInventoryName = new MutableLiveData<>();
+    private ListenerRegistration mRegistration;
+
+    public InventoryViewViewModel(String inventoryId) {
+
+        this.mInventoryId = inventoryId;
+
+        // Get name of inventory
+        mInventoryName.setValue(mRepository.getInventoryName(mInventoryId));
+    }
 
     public void addItem(Item item) {
 
-        mRepository.addItemToInventory(, )
+    }
+
+    public LiveData<String> getInventoryName() {
+
+        return mInventoryName;
     }
 }
