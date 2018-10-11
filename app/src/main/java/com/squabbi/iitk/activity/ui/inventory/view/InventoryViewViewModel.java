82f69dcdafb8c4a006b0b49b94fe8336cc6e56dd
squabbi <squabbi@users.noreply.github.com>
2018-10-11 14:16:52 +1100

    Refactor and creation of Inventory views

---
+++ app/src/main/java/com/squabbi/iitk/activity/ui/inventory/view/InventoryViewViewModel.java
@@ -0,0 +1,7 @@
+package com.squabbi.iitk.activity.ui.inventory.view;
+
+import androidx.lifecycle.ViewModel;
+
+public class CapsuleViewViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
