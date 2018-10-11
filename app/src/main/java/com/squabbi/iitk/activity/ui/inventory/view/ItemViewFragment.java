630f5971e910fbbe3ce2d05a333298239a9716f0
squabbi <squabbi@users.noreply.github.com>
2018-10-11 14:17:44 +1100

    Removed template, added ItemViewFragment viewmodel

--- app/src/main/java/com/squabbi/iitk/activity/ui/inventory/view/ItemViewFragment.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/inventory/view/ItemViewFragment.java
@@ -1,60 +1,38 @@
 package com.squabbi.iitk.activity.ui.inventory.view;
 
 
-
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.squabbi.iitk.R;
+import com.squabbi.iitk.model.Item;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ItemViewFragment#newInstance} factory method to
  * create an instance of this fragment.
- *
  */
 public class ItemViewFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    InventoryViewViewModel mViewModel;
 
+    public static ItemViewFragment newInstance() {
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ItemViewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ItemViewFragment newInstance(String param1, String param2) {
-        ItemViewFragment fragment = new ItemViewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-    public ItemViewFragment() {
-        // Required empty public constructor
+        return new ItemViewFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mViewModel = ViewModelProviders.of(this).get(InventoryViewViewModel.class);
     }
 
     @Override
