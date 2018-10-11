3be0b292acb72024bb6c37b0c50fd81d9c6712c3
squabbi <squabbi@users.noreply.github.com>
2018-10-11 13:46:45 +1100

    Added Inventory View activity

---
+++ app/src/main/java/com/squabbi/iitk/activity/ui/inventory/view/InventoryViewActivity.java
@@ -0,0 +1,14 @@
+package com.squabbi.iitk.activity.ui.inventory.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InventoryViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_view);
+    }
+}
