14c1c4b6d0628b54fa4ad7cd55e582d5b1a3b1f2
squabbi <squabbi@users.noreply.github.com>
2018-10-12 18:15:26 +1100

    WIP: added add capsules fragment

--- app/src/main/java/com/squabbi/iitk/activity/ui/inventory/AddCapsulesFragment.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/inventory/AddCapsulesFragment.java
@@ -1,65 +1,45 @@
-package com.squabbi.iitk;
+package com.squabbi.iitk.activity.ui.inventory;
 
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.squabbi.iitk.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link AddCapsulesFragment.OnFragmentInteractionListener} interface
+ * {@link OnInventoryFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link AddCapsulesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class AddCapsulesFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private OnInventoryFragmentInteractionListener mListener;
+    private ManageInventoryViewModel mViewModel;
 
     public AddCapsulesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddCapsulesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddCapsulesFragment newInstance(String param1, String param2) {
-        AddCapsulesFragment fragment = new AddCapsulesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static AddCapsulesFragment newInstance() {
+        return new AddCapsulesFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // Register ViewModel
+        mViewModel = ViewModelProviders.of(getActivity()).get(ManageInventoryViewModel.class);
     }
 
     @Override
@@ -69,21 +49,14 @@ public class AddCapsulesFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_add_capsules, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnInventoryFragmentInteractionListener) {
+            mListener = (OnInventoryFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement OnInventoryFragmentInteractionListener");
         }
     }
 
@@ -91,20 +64,5 @@ public class AddCapsulesFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
