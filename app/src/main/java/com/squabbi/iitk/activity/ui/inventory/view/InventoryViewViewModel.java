9d1515c0a0d269f88a10cfa8cedf90d40885564e
squabbi <squabbi@users.noreply.github.com>
2018-10-12 12:09:39 +1100

    Created ViewModel for InventoryView

--- app/src/main/java/com/squabbi/iitk/activity/ui/inventory/view/InventoryViewViewModel.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/inventory/view/InventoryViewViewModel.java
@@ -1,8 +1,15 @@
 package com.squabbi.iitk.activity.ui.inventory.view;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
+import com.squabbi.iitk.model.Inventory;
 import com.squabbi.iitk.model.Item;
 import com.squabbi.iitk.util.FirebaseRepository;
+
+import javax.annotation.Nullable;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -13,24 +20,61 @@ public class InventoryViewViewModel extends ViewModel {
     private static final String TAG = "InventoryViewViewModel";
 
     private FirebaseRepository mRepository = FirebaseRepository.getInstance();
+    private String mInventoryPath;
     private String mInventoryId;
     private MutableLiveData<String> mInventoryName = new MutableLiveData<>();
-    private ListenerRegistration mRegistration;
+    private ListenerRegistration mInventoryRegistration;
+    private DocumentListener mInventoryListener = new DocumentListener();
 
-    public InventoryViewViewModel(String inventoryId) {
+    private FirestoreRecyclerOptions.Builder<Item> mBaseInventoryItemFirestoreRecyclerBuilder;
 
+    public InventoryViewViewModel(String inventoryPath, String inventoryId) {
+
+        this.mInventoryPath = inventoryPath;
         this.mInventoryId = inventoryId;
 
-        // Get name of inventory
-        mInventoryName.setValue(mRepository.getInventoryName(mInventoryId));
+        this.mBaseInventoryItemFirestoreRecyclerBuilder = new FirestoreRecyclerOptions.Builder<Item>()
+                .setQuery(mRepository.getInventoryItemDocuments(mInventoryId), Item.class);
+
+        // Create Inventory object from ID, listen to changes to the document
+        mInventoryRegistration = mRepository.getDocumentRefObject(mInventoryPath)
+                .addSnapshotListener(mInventoryListener);
+    }
+
+    private class DocumentListener implements EventListener<DocumentSnapshot> {
+
+        @Override
+        public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+
+            if (e != null) {
+                // A Firebase error occurred
+                e.printStackTrace();
+                return;
+            }
+
+            if (documentSnapshot != null) {
+                Inventory inventory = documentSnapshot.toObject(Inventory.class);
+
+                if (inventory != null) {
+                    mInventoryName.setValue(inventory.getName());
+                }
+            }
+        }
+    }
+
+    public FirestoreRecyclerOptions.Builder<Item> getBaseItemFirestoreRecyclerBuilder() {
+        return mBaseInventoryItemFirestoreRecyclerBuilder;
     }
 
     public void addItem(Item item) {
-
+        mRepository.addItemToInventory(mInventoryId, item);
     }
 
     public LiveData<String> getInventoryName() {
-
         return mInventoryName;
+    }
+
+    public String getInventoryId() {
+        return mInventoryId;
     }
 }
