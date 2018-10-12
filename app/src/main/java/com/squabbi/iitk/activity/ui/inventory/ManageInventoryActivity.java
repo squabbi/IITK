4ab243a80cdf5fb1f75f4695ace9c5eba47680ae
squabbi <squabbi@users.noreply.github.com>
2018-10-12 18:17:01 +1100

    Implemented pager adapter

--- app/src/main/java/com/squabbi/iitk/activity/ui/inventory/ManageInventoryActivity.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/inventory/ManageInventoryActivity.java
@@ -3,8 +3,14 @@ package com.squabbi.iitk.activity.ui.inventory;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import butterknife.BindView;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
@@ -13,7 +19,6 @@ import butterknife.OnClick;
 import android.content.Context;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,61 +27,96 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.squabbi.iitk.R;
 import com.squabbi.iitk.activity.ui.inventory.view.InventoryViewActivity;
 import com.squabbi.iitk.databinding.ActivityManageInventoryBinding;
 import com.squabbi.iitk.model.Item;
 import com.squabbi.iitk.model.Weapon;
-import com.squabbi.iitk.util.Constants;
 import com.squabbi.iitk.util.ViewModelFactory;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class ManageInventoryActivity extends AppCompatActivity {
+public class ManageInventoryActivity extends AppCompatActivity implements OnInventoryFragmentInteractionListener {
 
     private ManageInventoryViewModel mViewModel;
-    private Integer mColour;
 
-    private static final String TAG_CHROMA_DIALOG = "TAG_CHROMA_DIALOG";
+    private static final int NUM_PAGES = 8;
+    private PagerAdapter mPagerAdapter;
 
-    @BindView(R.id.new_inventory_toolbar)
+    @BindView(R.id.add_inventory_items_viewpager)
+    ViewPager mViewPager;
+
+    @BindView(R.id.add_inventory_items_toolbar)
     Toolbar mToolbar;
 
-    @BindView(R.id.new_inventory_name_edittext)
-    EditText mNameEt;
+    @Override
+    public void onItemSelected(DocumentSnapshot documentSnapshot, int position) {
 
-    // Array of Locker ImageViews
-    @BindViews({ R.id.new_inv_green_locker_imageview, R.id.new_inv_blue_locker_imageview,
-            R.id.new_inv_white_locker_imageview, R.id.new_inv_red_locker_imageview, R.id.new_inv_yellow_locker_imageview,
-            R.id.new_inv_anniversary_locker_imageview })
-    List<ImageView> mLockerImageViews;
+    }
 
-    // Set onClickListener for Locker ImageViews
-    @OnClick({ R.id.new_inv_green_locker_imageview, R.id.new_inv_blue_locker_imageview,
-            R.id.new_inv_white_locker_imageview, R.id.new_inv_red_locker_imageview, R.id.new_inv_yellow_locker_imageview,
-            R.id.new_inv_anniversary_locker_imageview })
-    public void selectLocker(ImageView imageView) {
+    @Override
+    public void onViewPressed(View view) {
 
-        switch (imageView.getId()) {
-            case R.id.new_inv_green_locker_imageview:
-                mViewModel.setSelectedCapsule(0, !mViewModel.getSelectedCapsule(0));
-                break;
-            case R.id.new_inv_blue_locker_imageview:
-                mViewModel.setSelectedCapsule(1, !mViewModel.getSelectedCapsule(1));
-                break;
-            case R.id.new_inv_white_locker_imageview:
-                mViewModel.setSelectedCapsule(2, !mViewModel.getSelectedCapsule(2));
-                break;
-            case R.id.new_inv_red_locker_imageview:
-                mViewModel.setSelectedCapsule(3, !mViewModel.getSelectedCapsule(3));
-                break;
-            case R.id.new_inv_yellow_locker_imageview:
-                mViewModel.setSelectedCapsule(4, !mViewModel.getSelectedCapsule(4));
-                break;
-            case R.id.new_inv_anniversary_locker_imageview:
-                mViewModel.setSelectedCapsule(5, !mViewModel.getSelectedCapsule(5));
-                break;
+    }
+
+//    // Array of Locker ImageViews
+//    @BindViews({ R.id.new_inv_green_locker_imageview, R.id.new_inv_blue_locker_imageview,
+//            R.id.new_inv_white_locker_imageview, R.id.new_inv_red_locker_imageview, R.id.new_inv_yellow_locker_imageview,
+//            R.id.new_inv_anniversary_locker_imageview })
+//    List<ImageView> mLockerImageViews;
+//
+//    // Set onClickListener for Locker ImageViews
+//    @OnClick({ R.id.new_inv_green_locker_imageview, R.id.new_inv_blue_locker_imageview,
+//            R.id.new_inv_white_locker_imageview, R.id.new_inv_red_locker_imageview, R.id.new_inv_yellow_locker_imageview,
+//            R.id.new_inv_anniversary_locker_imageview })
+//    public void selectLocker(ImageView imageView) {
+//
+//        switch (imageView.getId()) {
+//            case R.id.new_inv_green_locker_imageview:
+//                mViewModel.setSelectedCapsule(0, !mViewModel.getSelectedCapsule(0));
+//                break;
+//            case R.id.new_inv_blue_locker_imageview:
+//                mViewModel.setSelectedCapsule(1, !mViewModel.getSelectedCapsule(1));
+//                break;
+//            case R.id.new_inv_white_locker_imageview:
+//                mViewModel.setSelectedCapsule(2, !mViewModel.getSelectedCapsule(2));
+//                break;
+//            case R.id.new_inv_red_locker_imageview:
+//                mViewModel.setSelectedCapsule(3, !mViewModel.getSelectedCapsule(3));
+//                break;
+//            case R.id.new_inv_yellow_locker_imageview:
+//                mViewModel.setSelectedCapsule(4, !mViewModel.getSelectedCapsule(4));
+//                break;
+//            case R.id.new_inv_anniversary_locker_imageview:
+//                mViewModel.setSelectedCapsule(5, !mViewModel.getSelectedCapsule(5));
+//                break;
+//        }
+//    }
+
+    private class InventoryItemsPagerAdapter extends FragmentStatePagerAdapter {
+
+        public InventoryItemsPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return AddCapsulesFragment.newInstance();
+                case 1:
+                    return AddModsFragment.newInstance();
+                case 2:
+                    return AddWeaponsFragment.newInstance();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
         }
     }
 
@@ -93,29 +133,11 @@ public class ManageInventoryActivity extends AppCompatActivity {
         ActivityManageInventoryBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_manage_inventory);
         binding.setViewModel(mViewModel);
         binding.setLifecycleOwner(this);
-
         ButterKnife.bind(this);
 
-        // Set initial status of Lockers
-        for (ImageView v : mLockerImageViews) {
-            setUnselected(v);
-        }
-
-        // Observe LiveData for selcted lockers
-        mViewModel.getSelectedCapsules().observe(this, new Observer<boolean[]>() {
-            @Override
-            public void onChanged(boolean[] booleans) {
-                // Set the checked status depending on array
-                for (int i = 0; i < Constants.LockerTypes.length; i++) {
-                    if (booleans[i]) {
-                        setSelected(mLockerImageViews.get(i));
-                    } else {
-                        // Unhighlight the capsule
-                        setUnselected(mLockerImageViews.get(i));
-                    }
-                }
-            }
-        });
+        // Instantiate ViewPager and PagerAdapter
+        mPagerAdapter = new InventoryItemsPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
 
         setSupportActionBar(mToolbar);
 
@@ -139,11 +161,6 @@ public class ManageInventoryActivity extends AppCompatActivity {
             case R.id.menu_done:
                 // Close the keyboard and submit strings to ViewModel.
                 closeKeyboard();
-                // Validate entries
-                if (validateText()) {
-
-                }
-
                 // Add portal and exit activity
                 addInventory();
                 finish();
@@ -162,19 +179,6 @@ public class ManageInventoryActivity extends AppCompatActivity {
             InputMethodManager iMm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
             iMm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
-    }
-
-    private boolean validateText() {
-        boolean passingStatus = true;
-
-        // Check for empty name
-        if (isEmpty(mNameEt)) {
-            //mNameInputLayout.setError(getString(R.string.inputlayout_error_emptyname));
-            //mNameInputLayout.setErrorEnabled(true);
-            passingStatus = false;
-        }
-
-        return passingStatus;
     }
 
     private void addInventory() {
