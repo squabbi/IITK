1b30413bffdb94cd55a25b37b413f8b212dbf470
squabbi <squabbi@users.noreply.github.com>
2018-10-14 10:31:14 +1100

    Replaced capsule messages, removed colour methods and fields

--- app/src/main/java/com/squabbi/iitk/activity/ui/inventory/manage/ManageInventoryViewModel.java
+++ app/src/main/java/com/squabbi/iitk/activity/ui/inventory/manage/ManageInventoryViewModel.java
@@ -1,14 +1,14 @@
-package com.squabbi.iitk.activity.ui.inventory;
+package com.squabbi.iitk.activity.ui.inventory.manage;
 
 import android.graphics.Color;
 
 import com.kunzisoft.androidclearchroma.ChromaDialog;
 import com.kunzisoft.androidclearchroma.IndicatorMode;
 import com.kunzisoft.androidclearchroma.colormode.ColorMode;
+import com.squabbi.iitk.model.InventoryItem;
 import com.squabbi.iitk.model.Item;
-import com.squabbi.iitk.model.KeyLocker;
-import com.squabbi.iitk.util.Constants;
 import com.squabbi.iitk.util.FirebaseRepository;
+import com.squabbi.iitk.util.InventoryItemConverter;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -20,34 +20,47 @@ import androidx.lifecycle.ViewModel;
 public class ManageInventoryViewModel extends ViewModel {
 
     private FirebaseRepository mFirebaseRepository = FirebaseRepository.getInstance();
-    private MutableLiveData<boolean[]> mSelectedCapsulesLiveData = new MutableLiveData<>();
-    private boolean[] mSelectedCapsules = new boolean[6];
     private MutableLiveData<Integer> mColorLiveData = new MutableLiveData<>();
+
+    private MutableLiveData<List<InventoryItem>> mInventoryCheckoutLiveData = new MutableLiveData<>();
+    private List<InventoryItem> mInventoryCartItems;
     private String mInventoryId;
 
     public ManageInventoryViewModel(String inventoryId) {
 
+        this.mInventoryCartItems = new LinkedList<>();
         this.mInventoryId = inventoryId;
-        // Set default colour
-        setColorLiveData(Color.WHITE);
     }
 
-    public void addInventory(String name, String description) {
+    public List<InventoryItem> getInventoryCartItems() {
+        return mInventoryCartItems;
+    }
 
-        Constants.LockerType[] lockerTypes = Constants.LockerType.values();
+    public MutableLiveData<List<InventoryItem>> getInventoryCheckoutLiveData() {
+        return mInventoryCheckoutLiveData;
+    }
 
-        // Generate items list
-        List<Item> items = new LinkedList<>();
-        for (int i = 0; i < mSelectedCapsules.length; i++) {
-            if (mSelectedCapsules[i]) {
-                items.add(new KeyLocker(lockerTypes[i], String.valueOf(i)));
-            }
+    public void addItemToInventoryCart(InventoryItem item) {
+        mInventoryCartItems.add(item);
+        mInventoryCheckoutLiveData.setValue(mInventoryCartItems);
+    }
+
+    public void removeItemFromInventoryCart(int position) {
+        mInventoryCartItems.remove(position);
+        mInventoryCheckoutLiveData.setValue(mInventoryCartItems);
+    }
+
+    public void removeAllFromCart() {
+        mInventoryCartItems.clear();
+        mInventoryCheckoutLiveData.setValue(mInventoryCartItems);
+    }
+
+    public void checkoutCart() {
+        // Iterate through InventoryCartItems
+        for (InventoryItem item : mInventoryCartItems) {
+            // Determine a respective Item object
+            mFirebaseRepository.addItemToInventory(mInventoryId, InventoryItemConverter.determineItem(item));
         }
-
-//        // Create inventory object
-//        Inventory inventory = new Inventory(name, description,
-//                getColourLiveData().getValue(), items);
-//        mFirebaseRepository.addInventory(inventory);
     }
 
     public void addItemsToInventory(List<Item> items) {
@@ -66,24 +79,7 @@ public class ManageInventoryViewModel extends ViewModel {
                 .indicatorMode(IndicatorMode.HEX);
     }
 
-    public void setSelectedCapsule(int capsule, boolean state) {
-
-        mSelectedCapsules[capsule] = state;
-        // Update the LiveData
-        mSelectedCapsulesLiveData.setValue(mSelectedCapsules);
-    }
-
-    public boolean getSelectedCapsule(int capsule) {
-        return mSelectedCapsules[capsule];
-    }
-
-    public LiveData<boolean[]> getSelectedCapsules() {
-        return mSelectedCapsulesLiveData;
-    }
-
     public LiveData<Integer> getColourLiveData() { return mColorLiveData; }
 
     public void setColorLiveData(Integer colour) { mColorLiveData.setValue(colour); }
-
-
 }
