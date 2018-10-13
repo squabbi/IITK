7317e43b4b27bde8c8078f88d9b3ab998b714b6c
squabbi <squabbi@users.noreply.github.com>
2018-10-14 10:38:09 +1100

    Created inventory item to hold details for population of RecyclerViews

--- app/src/main/java/com/squabbi/iitk/model/InventoryItem.java
+++ app/src/main/java/com/squabbi/iitk/model/InventoryItem.java
@@ -1,24 +1,36 @@
 package com.squabbi.iitk.model;
 
-public class Mod {
+public class InventoryItem {
 
-    private String mName;
+    private Item.DetailItemType mDetailItemType;
+    private Integer mNameResource;
     private Item.Rarity mRarity;
+    private Integer mLevel;
     private Integer mImageResource;
 
-    public Mod(String name, Item.Rarity rarity, Integer imageResource) {
+    public InventoryItem(Item.DetailItemType detailItemType, Integer name, Item.Rarity rarity, Integer level, Integer imageResource) {
 
-        this.mName = name;
+        this.mDetailItemType = detailItemType;
+        this.mNameResource = name;
         this.mRarity = rarity;
+        this.mLevel = level;
         this.mImageResource = imageResource;
     }
 
-    public String getName() {
-        return mName;
+    public Item.DetailItemType getDetailItemType() {
+        return mDetailItemType;
+    }
+
+    public Integer getNameResource() {
+        return mNameResource;
     }
 
     public Item.Rarity getRarity() {
         return mRarity;
+    }
+
+    public Integer getLevel() {
+        return mLevel;
     }
 
     public Integer getImageResource() {
