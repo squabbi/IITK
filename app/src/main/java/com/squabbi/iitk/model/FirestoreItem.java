f2348315b46ca9b839cf0500d2f65751812777df
squabbi <squabbi@users.noreply.github.com>
2018-10-14 10:35:36 +1100

    Created megaclass FirestoreItem

--- app/src/main/java/com/squabbi/iitk/model/FirestoreItem.java
+++ app/src/main/java/com/squabbi/iitk/model/FirestoreItem.java
@@ -1,124 +1,59 @@
 package com.squabbi.iitk.model;
 
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.IgnoreExtraProperties;
+import com.squabbi.iitk.util.Constants;
+import com.squabbi.iitk.model.Item.ItemType;
+import com.squabbi.iitk.model.Item.Rarity;
 
 @IgnoreExtraProperties
-public class Item {
+public class FirestoreItem {
 
+    // Super item fields
     private ItemType mItemType;
     private Rarity mRarity;
     private Integer mLevel;
 
-    // Empty constructor for Firestore
-    Item() {}
+    // Weapon fields
+    private Weapon.WeaponType mWeaponType;
 
-    Item (ItemType itemType,  Rarity rarity, Integer level) {
+    // Mod fields
+    private boolean mIsSbul;
+    private boolean mIsAegis;
+    private Mod.ModType mModType;
+
+    // Capsule fields
+    private Capsule.CapsuleType mCapsuleType;
+    private String mCapsuleId;
+
+    // FlipCard fields
+    private FlipCard.FlipType mFlipType;
+
+    // Key Locker fields
+    private Constants.LockerType mLockerType;
+
+    // Portal Key fields
+    private String mPortalName;
+    private String mPortalLocation;
+    private DocumentReference mPortalReference;
+
+    // Powercube fields
+    private boolean mIsLawson;
+
+    // Powerup fields
+    private Powerup.PowerupType mPowerupType;
+
+    // Empty constructor for Firestore
+    FirestoreItem() {}
+
+    FirestoreItem(ItemType itemType, Rarity rarity, Integer level) {
 
         this.mItemType = itemType;
         this.mRarity = rarity;
         this.mLevel = level;
     }
 
-    public enum ItemType {
-        WEAPON {
-            @Override
-            public String toString() {
-                return "Weapon";
-            }
-        }, MOD {
-            @Override
-            public String toString() {
-                return "Mod";
-            }
-        }, KEY {
-            @Override
-            public String toString() {
-                return "Portal Key";
-            }
-        }, KEY_LOCKER {
-            @Override
-            public String toString() {
-                return "Key Locker";
-            }
-        }, CAPSULE {
-            @Override
-            public String toString() {
-                return "Capsule";
-            }
-        }, POWER_CUBE {
-            @Override
-            public String toString() {
-                return "Power Cube";
-            }
-        }, POWERUP {
-            @Override
-            public String toString() {
-                return "Powerup";
-            }
-        }, RESONATOR {
-            @Override
-            public String toString() {
-                return "Resonator";
-            }
-        }
-    }
-
-    public enum DetailItemType {
-
-        RESONATOR,
-        PORTAL_KEY,
-        SHIELD,
-        AEGIS_SHIELD,
-        FORCE_AMP,
-        LINK_AMP,
-        SOFTBANK_UL,
-        MULTI_HACK,
-        HEAT_SINK,
-        TURRET,
-        ITO_EN_P,
-        ITO_EN_M,
-        XMP,
-        ULTRA_STRIKE,
-        ADA,
-        JARVIS,
-        POWER_CUBE,
-        LAWSON,
-        CAPSULE,
-        QUANTUM,
-        LOCKER_GREEN,
-        LOCKER_BLUE,
-        LOCKER_WHITE,
-        LOCKER_RED,
-        LOCKER_YELLOW,
-        LOCKER_ANNIVERSARY,
-        FRACKER,
-        BEACON
-    }
-
-    public enum Rarity {
-        VERY_COMMON {
-            @Override
-            public String toString() {
-                return "Very Common";
-            }
-        }, COMMON {
-            @Override
-            public String toString() {
-                return "Common";
-            }
-        }, RARE {
-            @Override
-            public String toString() {
-                return "Rare";
-            }
-        }, VERY_RARE {
-            @Override
-            public String toString() {
-                return "Very Rare";
-            }
-        }
-    }
-
+    // Stock Item getters and setters
     public ItemType getItemType() {
         return mItemType;
     }
@@ -141,5 +76,117 @@ public class Item {
 
     public void setLevel(Integer level) {
         mLevel = level;
+    }
+
+    // Weapon getters/setters
+    public Weapon.WeaponType getWeaponType() {
+        return mWeaponType;
+    }
+
+    public void setWeaponType(Weapon.WeaponType weaponType) {
+        mWeaponType = weaponType;
+    }
+
+    // Mod getters/setters
+    public Mod.ModType getModType() {
+        return mModType;
+    }
+
+    public void setModType(Mod.ModType modType) {
+        mModType = modType;
+    }
+
+    public boolean isSbul() {
+        return mIsSbul;
+    }
+
+    public void setSbul(boolean sbul) {
+        mIsSbul = sbul;
+    }
+
+    public boolean isAegis() {
+        return mIsAegis;
+    }
+
+    public void setAegis(boolean aegis) {
+        mIsAegis = aegis;
+    }
+
+    // Capsule getters/setters
+    public Capsule.CapsuleType getCapsuleType() {
+        return mCapsuleType;
+    }
+
+    public void setCapsuleType(Capsule.CapsuleType capsuleType) {
+        mCapsuleType = capsuleType;
+    }
+
+    public String getCapsuleId() {
+        return mCapsuleId;
+    }
+
+    public void setCapsuleId(String capsuleId) {
+        mCapsuleId = capsuleId;
+    }
+
+    // Flipcard setters/getters
+    public FlipCard.FlipType getFlipType() {
+        return mFlipType;
+    }
+
+    public void setFlipType(FlipCard.FlipType flipType) {
+        mFlipType = flipType;
+    }
+
+    // Key Locker getters/setters
+    public Constants.LockerType getLockerType() {
+        return mLockerType;
+    }
+
+    public void setLockerType(Constants.LockerType lockerType) {
+        mLockerType = lockerType;
+    }
+
+    // Portal key getters/setters
+    public String getPortalName() {
+        return mPortalName;
+    }
+
+    public void setPortalName(String portalName) {
+        mPortalName = portalName;
+    }
+
+    public String getPortalLocation() {
+        return mPortalLocation;
+    }
+
+    public void setPortalLocation(String portalLocation) {
+        mPortalLocation = portalLocation;
+    }
+
+    public DocumentReference getPortalReference() {
+        return mPortalReference;
+    }
+
+    public void setPortalReference(DocumentReference portalReference) {
+        mPortalReference = portalReference;
+    }
+
+    // Powercube getters/setters
+    public boolean isLawson() {
+        return mIsLawson;
+    }
+
+    public void setLawson(boolean lawson) {
+        mIsLawson = lawson;
+    }
+
+    // Powerup getters/setters
+    public Powerup.PowerupType getPowerupType() {
+        return mPowerupType;
+    }
+
+    public void setPowerupType(Powerup.PowerupType powerupType) {
+        mPowerupType = powerupType;
     }
 }
