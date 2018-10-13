80aa14b301e3bc6452c9efd857a3584a54989e6e
squabbi <squabbi@users.noreply.github.com>
2018-10-14 10:26:43 +1100

    Pushed SBUL logic to the Mod class

--- app/src/main/java/com/squabbi/iitk/model/SoftBankUltraLink.java
+++ app/src/main/java/com/squabbi/iitk/model/SoftBankUltraLink.java
@@ -2,12 +2,7 @@ package com.squabbi.iitk.model;
 
 public class SoftBankUltraLink extends Mod {
 
-    private boolean isSbul;
-
-    // Empty constructor required by Firebase
-    public SoftBankUltraLink() {}
-
-    public SoftBankUltraLink(boolean isSbul) {
-        super(Rarity.VERY_RARE, ModType.LINK_AMP, isSbul);
+    public SoftBankUltraLink() {
+        super(Rarity.VERY_RARE, ModType.LINK_AMP, true, false);
     }
 }
