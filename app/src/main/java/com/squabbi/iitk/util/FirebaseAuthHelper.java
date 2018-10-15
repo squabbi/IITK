b397171991c0116be090a319961545dfa1e8dbcf
squabbi <squabbi@users.noreply.github.com>
2018-10-16 09:05:20 +1100

    Implemented singleton for Firebase Authentication

--- app/src/main/java/com/squabbi/iitk/util/FirebaseAuthHelper.java
+++ app/src/main/java/com/squabbi/iitk/util/FirebaseAuthHelper.java
@@ -1,4 +1,24 @@
 package com.squabbi.iitk.util;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class FirebaseAuthHelper {
+
+    private static FirebaseAuthHelper mAuthHelper;
+    private FirebaseAuth mAuth;
+
+    public static FirebaseAuthHelper getInstance() {
+        if (mAuthHelper == null) {
+            mAuthHelper = new FirebaseAuthHelper();
+        }
+        return mAuthHelper;
+    }
+
+    private FirebaseAuthHelper() {
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public FirebaseAuth getAuth() {
+        return mAuth;
+    }
 }
